a03526dbf2c96ff44f59b8c41f8051c292b63e6e
Alex Kaz <you@example.com>
2025-10-19 16:45:43 +0700

    new task3

--- src/advanced_array_function.c
+++ src/advanced_array_function.c
@@ -46,64 +46,44 @@ int length_of_lis(int* nums, int numsSize) {
 int* merge(int* intervals, int intervalsSize, int* returnSize) {
     if (intervalsSize == 0) {
         *returnSize = 0;
-        return NULL;
+        return 0;
     }
-    
-    // bubble sort
-    for (int i = 0; i < intervalsSize - 1; i++) {
-        for (int j = 0; j < intervalsSize - i - 1; j++) {
-            if (intervals[j * 2] > intervals[(j + 1) * 2]) {
 
-                int tempStart = intervals[j * 2];
-                int tempEnd = intervals[j * 2 + 1];
-                
+    int n = intervalsSize / 2;
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (intervals[j * 2] > intervals[(j + 1) * 2]) {
+                int a = intervals[j * 2];
+                int b = intervals[j * 2 + 1];
                 intervals[j * 2] = intervals[(j + 1) * 2];
                 intervals[j * 2 + 1] = intervals[(j + 1) * 2 + 1];
-                
-                intervals[(j + 1) * 2] = tempStart;
-                intervals[(j + 1) * 2 + 1] = tempEnd;
+                intervals[(j + 1) * 2] = a;
+                intervals[(j + 1) * 2 + 1] = b;
             }
         }
     }
-    
-    // временный массив для результатика
-    int tempResult = (int)malloc(intervalsSize * sizeof(int*));
-    for (int i = 0; i < intervalsSize; i++) {
-        tempResult[i] = (int*)malloc(2 * sizeof(int));
-    }
-    
-    int resultSize = 0;
-    tempResult[resultSize][0] = intervals[0];
-    tempResult[resultSize][1] = intervals[1];
-    
-    // алгоритм объединения
-    for (int i = 1; i < intervalsSize; i++) {
-        int currentStart = intervals[i * 2];
-        int currentEnd = intervals[i * 2 + 1];
-        int lastEnd = tempResult[resultSize][1];
-       
-        if (currentStart <= lastEnd) {
-            if (currentEnd > lastEnd) {
-                tempResult[resultSize][1] = currentEnd;
-            }
+
+    int* res = malloc(intervalsSize * sizeof(int));
+    int k = 0;
+    int start = intervals[0];
+    int end = intervals[1];
+
+    for (int i = 2; i < intervalsSize; i += 2) {
+        int s = intervals[i];
+        int e = intervals[i + 1];
+        if (s <= end) {
+            if (e > end)
+                end = e;
         } else {
-            resultSize++;
-            tempResult[resultSize][0] = currentStart;
-            tempResult[resultSize][1] = currentEnd;
+            res[k++] = start;
+            res[k++] = end;
+            start = s;
+            end = e;
         }
     }
-    
-    resultSize++;
-    
-    // Теперь все в один массив
-    int* flatResult = (int*)malloc(resultSize * 2 * sizeof(int));
-    for (int i = 0; i < resultSize; i++) {
-        flatResult[i * 2] = tempResult[i][0];
-        flatResult[i * 2 + 1] = tempResult[i][1];
-        free(tempResult[i]);
-    }
-    free(tempResult);
-    
-    *returnSize = resultSize;
-    return flatResult;
+
+    res[k++] = start;
+    res[k++] = end;
+    *returnSize = k;
+    return res;
 }
